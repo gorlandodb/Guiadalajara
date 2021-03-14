27f860161a1cebe85aae80192d605e11d51e1a4b
G. Orlando Durán <gorlandodb@icloud.com>
2021-03-14 02:14:36 -0600

    OG Meta Tags

--- pages/index.js
+++ pages/index.js
@@ -16,11 +16,29 @@ export default function Home() {
         <meta name="apple-mobile-web-app-capable" content="yes">
         <meta name="application-name" content="Acropolis">
         <meta name="apple-mobile-web-app-title" content="Acropolis">
-        <meta name="theme-color" content="#333333">
         <meta name="msapplication-navbutton-color" content="#333333">
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
         <meta name="msapplication-starturl" content="/">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"></meta>
+        <meta name="msapplication-TileColor" content="#ffffff" />
+      
+        <meta name="theme-color" content="#ffffff" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta httpEquiv="Content-Language" content="es" />
+        <meta name="description" content="Acrópolis Digital: Reinventamos tu mundo digital" />
+        <meta name="og:description" content="Acrópolis Digital: Reinventamos tu mundo digital" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:image" href="/og.png" />
+        <meta name="twitter:site:domain" content="nextra.vercel.app" />
+        <meta name="twitter:url" content="https://registro.acropolis.digital" />
+        <meta name="og:title" content="Acrópolis Digital: Reinventamos tu mundo digital" />
+        <meta name="og:image" href="/og.png" />
+        <meta name="apple-mobile-web-app-title" content="Acrópolis Digital - Docs" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-icon-180x180.png"/>
+        <link rel="icon" type="image/png" sizes="192x192"  href="/android-icon-192x192.png"/>
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+        <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+        <meta name="msapplication-TileImage" content="/ms-icon-144x144.png"/>
       </Head>
 
       <main className={styles.main}>
